c1f8eea66f5ddab657f072cee7a65a5277ef6ca2
xyq <xyq@localhost.localdomain>
2016-12-09 18:11:42 +0800

    add

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,16 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -23,4 +28,8 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    login_manager.init_app(app)
     return app
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?',validators=[Required()])
     submit = SubmitField('Submit')
