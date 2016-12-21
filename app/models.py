c925d7924da53f9c94502ce2f528584132394ddd
xyq946692052 <xyq2312@sina.com>
2016-12-22 00:12:39 +0800

    add

--- app/models.py
+++ app/models.py
@@ -63,3 +63,15 @@ class User(UserMixin,db.Model):
     def generate_reset_token(self,expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'])
         return s.dumps({'reset':self.id})
+
+    def reset_password(self,token,new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        return True
