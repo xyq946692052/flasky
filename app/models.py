46de9a5c0a287380518b63d3a163456cd8ba73c4
xyq946692052 <xyq2312@sina.com>
2016-12-22 00:58:34 +0800

    add

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+import hashlib
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -73,5 +74,28 @@ class User(UserMixin,db.Model):
         if data.get('reset') != self.id:
             return False
         self.password = new_password
+        db.session.add(self)
+        return True
+
+    def generate_email_change_token(self,new_email,expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'change_email':self.id,'new_email':new_email})
+
+    def change_email(self,token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        self.avatar_hash = hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
